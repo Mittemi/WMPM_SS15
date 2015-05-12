11b911c849b402731645f2b69e62f80b03da76e1
Constantin Fränkel <constantin5000@gmx.net>
2015-05-12 20:05:37 +0200

    Test reading from csv file,bindy csv to bean conversion, split eip for Claims

---
+++ code/src/main/java/carrental/routing/CarInspectionRoute.java
@@ -0,0 +1,24 @@
+package carrental.routing;
+
+import carrental.beans.DemoPrintTextBean;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.BindyType;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Constantin on 12.05.2015.
+ */
+@Component
+public class CarInspectionRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+        from("file://claimlists?noop=true").split(body().tokenize("\n"))
+                .log("Line: ${body}")
+                .unmarshal()
+                .bindy(BindyType.Csv, "carrental.model")
+                .to("mock:update");
+    }
+}
