37336a79baf45c4e2986fb643b6fe6a4a71d6291
Alexander Eigner <e1128978@student.tuwien.ac.at>
2015-05-20 16:17:30 +0200

    Creation of Invoice-Objects in BillingProcessor implemented (first try)

--- code/src/main/java/carrental/routing/billing/BillingRoute.java
+++ code/src/main/java/carrental/routing/billing/BillingRoute.java
@@ -1,13 +1,20 @@
 package carrental.routing.billing;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+import carrental.beans.billing.BillingProcessor;
+import carrental.beans.returnPoint.CarInspectionProcessor;
+/**
+ * Created by Alexander on 20.05.2015
+ */
+@Component
 public class BillingRoute  extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
 		// TODO Auto-generated method stub
-		
+		 from("direct:endpoint").process(new BillingProcessor());
 	}
 
 }
