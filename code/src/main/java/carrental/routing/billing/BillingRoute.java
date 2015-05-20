44a87907ca374689789cf4ea227f756cc5324433
Alexander Eigner <e1128978@student.tuwien.ac.at>
2015-05-20 15:09:24 +0200

    created BillingRoute.java

---
+++ code/src/main/java/carrental/routing/billing/BillingRoute.java
@@ -0,0 +1,13 @@
+package carrental.routing.billing;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class BillingRoute  extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
