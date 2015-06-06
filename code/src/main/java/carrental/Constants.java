0e4ca4137378507b6760d3539f306c729065266c
Alexander Eigner <e1128978@student.tuwien.ac.at>
2015-06-06 12:39:32 +0200

    Implemented service-Cost calculation. Extended the Constant-class with service-cost-constants.

--- code/src/main/java/carrental/Constants.java
+++ code/src/main/java/carrental/Constants.java
@@ -1,5 +1,10 @@
 package carrental;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import carrental.model.pickupPoint.ClaimType;
+
 /**
  * Created by Michael on 13.05.2015.
  */
@@ -9,4 +14,14 @@ public class Constants {
     public static final boolean ENABLE_CAR_RETURN = false;
 
     public static final boolean ENABLE_CANCEL_PICKUP = false;
+    
+    public static final Map<Enum,Double> servicePrices=new HashMap<Enum,Double>();
+    
+    static{
+    	servicePrices.put(ClaimType.Paintwork, 350.50);
+    	servicePrices.put(ClaimType.Mechanical, 632.39);
+    	servicePrices.put(ClaimType.Electrical, 577.82);
+    	servicePrices.put(ClaimType.Cleaning, 132.61);
+    	servicePrices.put(ClaimType.Refilling, 60.54);
+    }
 }
